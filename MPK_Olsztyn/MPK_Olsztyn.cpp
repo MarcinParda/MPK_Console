9e088f4eff111bce61fc566a052962ce2e4dda47
mpardaedu <marcin98parda@wp.pl>
2018-01-21 16:00:41 +0100

    add 3 adding functions

--- MPK_Olsztyn/MPK_Olsztyn.cpp
+++ MPK_Olsztyn/MPK_Olsztyn.cpp
@@ -81,7 +81,8 @@ void wyswietl_menu_glowne()
 		cout << "6. Wyswietl mape miasta" << endl;
 		cout << "7. Dodaj nowy przystanek" << endl;
 		cout << "8. Dodaj nowa linie" << endl;
-		cout << "9. Wyloguj sie" << endl << endl;
+		cout << "9. Dodaj nowy rozklad przystanku" << endl;
+		cout << "0. Wyloguj sie" << endl << endl;
 	}
 }
 
@@ -104,6 +105,7 @@ public:
 	bool  czyCzynny;
 	int id;
 	vector<Rozklad>rozklady;
+
 	Przystanek(string t_nazwa, string t_adres, bool t_czyCzynny) {
 		licznik++;
 		id = licznik;
@@ -157,13 +159,13 @@ public:
 	int nr_linii;
 	int ile_przystankow;
 	vector<string>ktore_przystanki;
-	string typ_autobusu;
+	string srodek_komunikacji;
 
-	Linia(int t_nr_linii, int t_ile_przystankow, string t_typ_autobusu, vector<string>nazwy_przystankow) //podajemy nr linii, przez ile przystankow przejezdza, jaki jest typ autobusu
+	Linia(int t_nr_linii, int t_ile_przystankow, string t_srodek_komunikacji, vector<string>nazwy_przystankow) //podajemy nr linii, przez ile przystankow przejezdza, jaki jest srodek komunikacji
 	{
 		nr_linii = t_nr_linii;
 		ile_przystankow = t_ile_przystankow;
-		typ_autobusu = t_typ_autobusu;
+		srodek_komunikacji = t_srodek_komunikacji;
 		for (int i = 0; i < ile_przystankow; i++)
 		{
 			ktore_przystanki.push_back(nazwy_przystankow[i]);
@@ -209,7 +211,7 @@ public:
 	}
 
 	void wypisz() {
-		cout << nr_linii << " " << typ_autobusu << " ";
+		cout << nr_linii << " " << srodek_komunikacji << " ";
 		wyswietl(ktore_przystanki);
 		cout << endl;
 	}
@@ -224,7 +226,7 @@ public:
 	}
 
 	void wyswietl_liste_linii() {
-		cout << endl << "Nr  Typ autobusu Przystanki" << endl;
+		cout << endl << "Nr  Sr.kom. Przystanki" << endl;
 		for (int i = 0; i < linie.size(); i++)
 		{
 			linie[i].wypisz();
@@ -290,24 +292,27 @@ public:
 		string przystanek1, przystanek2;
 		cin >> przystanek1; 
 		cin >> przystanek2;
-
-		for (int i = 0; i < linie.size(); i++)
-		{
-			for (int j = 0; j < linie[i].ile_przystankow; j++)
+		if(przystanek1!=przystanek2) //nie sprawdzac dla takich samych przystankow
+			for (int i = 0; i < linie.size(); i++)
 			{
-				if (linie[i].ktore_przystanki[j] == przystanek1) {
-					for (int k = 0; k < linie[i].ile_przystankow; k++)
-					{
-						if (linie[i].ktore_przystanki[k] == przystanek2) {
-							if (!czyJestPoloczenie)
-								cout << "Przez te dwa przystanki jada linie nr: ";
-							cout << linie[i].nr_linii << " ";
-							czyJestPoloczenie = true;
+				for (int j = 0; j < linie[i].ile_przystankow; j++)
+				{
+					if (linie[i].ktore_przystanki[j] == przystanek1) {
+						for (int k = 0; k < linie[i].ile_przystankow; k++)
+						{
+							if (linie[i].ktore_przystanki[k] == przystanek2) {
+								if (!czyJestPoloczenie)
+									cout << "Przez te dwa przystanki jada linie nr: ";
+								cout << linie[i].nr_linii << " ";
+								czyJestPoloczenie = true;
+							}
 						}
 					}
 				}
 			}
-		}
+		else 
+			cout << endl << "Podano dwa takie same przystanki" << endl;
+
 		if (!czyJestPoloczenie)
 			cout << endl << "Nie ma polaczen miedzy przystankami" << endl;
 
@@ -315,6 +320,8 @@ public:
 
 	}
 };
+
+MPK MPK_Olsztyn("Olsztyn");
 
 bool logowanie() {
 
@@ -343,10 +350,93 @@ bool logowanie() {
 	return false;
 }
 
+Przystanek nowy_przystanek() {
+	Przystanek wynik("","",0);
+
+	cout << endl << "Podaj nazwe przystanku" << endl;
+	cin >> wynik.nazwa;
+
+	cout << endl << "Podaj adres przystanku" << endl;
+	cin >> wynik.adres;
+
+	cout << endl << "Czy przystanek jest czynny (Podaj 0 jesli nie lub 1 jesli tak)" << endl;
+	cin >> wynik.czyCzynny;
+
+	return wynik;
+}
+
+Linia nowa_linia() {
+	vector<string>nazwy_przystankow;
+	Linia wynik(0, 0, "", nazwy_przystankow);
+
+	cout << endl << "Podaj nr nowej linii" << endl;
+	cin >> wynik.nr_linii;
+
+	cout << endl << "Ile przystankow ma miec nowa linia" << endl;
+	cin >> wynik.ile_przystankow;
+	cout << endl << wynik.ile_przystankow << endl;
+
+	cout << endl << "Jaki srodek komunikacji posiada ta linia" << endl;
+	cin >> wynik.srodek_komunikacji;
+
+	for (int i = 0; i < wynik.ile_przystankow; i++)
+	{
+		string nazwa_przystanku;
+		if (i == 0)
+			cout << "Podaj przystanek poczatkowy" << endl;
+		else if(i==wynik.ile_przystankow-1)
+			cout << "Podaj przystanek koncowy" << endl;
+		else
+			cout << "Podaj przystanek nr " << i+1 << endl;
+
+		cin >> nazwa_przystanku;
+		wynik.ktore_przystanki.push_back(nazwa_przystanku);
+	}
+
+	return wynik;
+}
+
+void nowy_rozklad(string nazwa_przystanku) {
+	Rozklad rozklad;
+
+	cout << endl << "Jaka linia przejezdza przez ten przystanek" << endl;
+	cin >> rozklad.linia;
+
+	cout << endl << "O ktorej godzinie linia przejezdza przez ten przystanek" << endl;
+	cin >> rozklad.godzina_odjazdu;
+
+	cout << endl << "Czy jest to przystanek koncowy linii " << rozklad.linia <<  "? (Wpisz 1 jesli tak lub 0 jesli nie)" << endl;
+	bool yorn;
+	cin >> yorn;
+	if (yorn)
+		rozklad.w_ktora_strone = "Koncowy";
+	else {
+		for (int i = 0; i < MPK_Olsztyn.linie.size(); i++)
+		{
+			if (rozklad.linia == MPK_Olsztyn.linie[i].nr_linii) {
+				cout << "Jesli to glowna trasa wpisz 1 jesli powrotna wpisz 0" << endl;
+				cin >> yorn;
+				if (yorn) 
+					rozklad.w_ktora_strone = MPK_Olsztyn.linie[i].ktore_przystanki[MPK_Olsztyn.linie[i].ile_przystankow - 1]; //przypisuje ostatni przystanek danej linii zmiennej w_ktora_strone
+				else
+					rozklad.w_ktora_strone = MPK_Olsztyn.linie[i].ktore_przystanki[0]; //przypisuje pierwszy przystanek danej linii zmiennej w_ktora_strone
+				break;
+			}
+
+		}
+	}
+	//TO DO sprawdzic tam, gdzie przystanki, rozklady, linie moga nie istniec
+	for (int i = 0; i < przystanki.size(); i++)
+	{
+		if (nazwa_przystanku == przystanki[i].nazwa) {
+			przystanki[i].rozklady.push_back(rozklad);
+			break;
+		}
+	}
+}
+
 int main()
 {
-	MPK MPK_Olsztyn("Olsztyn");
-
 	//Odczyt danych z plikow
 
 	vector<string>dane_z_pliku_przystanki = wczytywanie_wyrazow_do_wektora("przystanki.txt"); //Wczytywanie danych z pliku przystanki txt i zapisywanie ich do tablicy obiektow przystanek
@@ -369,7 +459,7 @@ int main()
 		}
 		Linia linia(atoi(dane_z_pliku_linie[i].data()),   //nr linii
 			atoi(dane_z_pliku_linie[i + 1].data()), //ilosc przystankow
-			dane_z_pliku_linie[i + 2],			  //typ autobusu
+			dane_z_pliku_linie[i + 2],			  //srodek komunikacji
 			nazwy_przystankow);					  //jakie przystanki
 
 		MPK_Olsztyn.linie.push_back(linia);
@@ -414,9 +504,11 @@ int main()
 			break;
 		case '7':
 			zalogowano = logowanie();
+			wyswietl_menu_glowne();
+			znak = getchar();
 			if (zalogowano)
 			{
-				while (znak != '9') {
+				while (znak != '0') {
 					switch (znak) {
 					case '1':
 						MPK_Olsztyn.wyswietl_liste_linii();
@@ -437,6 +529,7 @@ int main()
 						MPK_Olsztyn.wyswietl_rozklad_przystanku();
 						czekaj_na_klawisz();
 						czekaj_na_klawisz();
+						break;
 					case '5':
 						MPK_Olsztyn.polaczenia();
 						czekaj_na_klawisz();
@@ -447,10 +540,23 @@ int main()
 						czekaj_na_klawisz();
 						break;
 					case '7':
+						przystanki.push_back(nowy_przystanek());
+						cout << endl << "Dodano nowy przystanek" << endl;
 						czekaj_na_klawisz();
 						czekaj_na_klawisz();
 						break;
 					case '8':
+						MPK_Olsztyn.linie.push_back(nowa_linia());
+						cout << endl << "Dodano nowa linie" << endl;
+						czekaj_na_klawisz();
+						czekaj_na_klawisz();
+						break;
+					case '9':
+						string nazwa_przystanku;
+						cout << endl << "Podaj nazwe przystanku do ktorego chcesz dodac nowy rozklad" << endl;
+						cin >> nazwa_przystanku;
+						nowy_rozklad(nazwa_przystanku);
+						cout << endl << "Dodano nowy rozklad" << endl;
 						czekaj_na_klawisz();
 						czekaj_na_klawisz();
 						break;
